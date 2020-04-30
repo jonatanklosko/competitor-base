1eea2ab4e7283a5a33c6b4e0d33b8f5e6753304e
Jonatan Kłosko <jonatanklosko@gmail.com>
2020-04-30 18:51:39 +0200

    Set up client.

---
+++ client/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+function App() {
+  return (
+    <Fragment>
+      <CssBaseline />
+      <div>
+        App
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
