62df37e0a8c1c676ee7cb1cd1581e58066878c73
Oliwia Masiarek <omoliweeek@gmail.com>
2020-05-02 01:10:28 +0200

    Change the default theme.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import { CssBaseline } from '@material-ui/core';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
 import Navigation from './Navigation';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#484848',
+      main: '#212121',
+      dark: '#000000',
+      contrastText: '#fff',
+    },
+  },
+});
+
 function App() {
   return (
-    <Router>
-      <CssBaseline />
-      <Navigation />
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <CssBaseline />
+        <Navigation />
+      </Router>
+    </ThemeProvider>
   );
 }
 
