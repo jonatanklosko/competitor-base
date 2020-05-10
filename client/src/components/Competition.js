7cc63858819e4d668b24ca76e763b1c5363488a9
Oliwia Masiarek <omoliweeek@gmail.com>
2020-05-10 16:49:43 +0200

    Add competition site.

--- client/src/components/Competition.js
+++ client/src/components/Competition.js
@@ -1,9 +1,76 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import {
+  LinearProgress,
+  Grid,
+  Typography,
+  ExpansionPanel,
+  ExpansionPanelSummary,
+  ExpansionPanelDetails,
+} from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { useQuery } from '../hooks/database';
+import FlagIcon from './FlagIcon';
+import RoundResultsTable from './RoundResultsTable';
+
+const query = `
+  MATCH
+    (c:Competition { wcaId: $wcaId }),
+    (c)-[organizedIn:ORGANIZED_IN]->(country:Country),
+    (c)<-[:HELD_IN]-(r:Round),
+    (r)-[:OF_EVENT]->(e:Event)
+  WITH c, organizedIn, country, r, e
+  ORDER BY e.name, r.roundNumber
+  RETURN
+    c AS competition,
+    organizedIn AS organizedIn,
+    country AS country,
+    collect({ round: r, event: e }) AS roundsWithEvent
+`;
 
 function Competition() {
   const { wcaId } = useParams();
-  return <div>Competition: {wcaId}</div>;
+  const { data, loading, error } = useQuery(query, { wcaId });
+
+  if (loading) return <LinearProgress />;
+  if (error) return error.message;
+  const [{ competition, organizedIn, country, roundsWithEvent }] = data;
+
+  return (
+    <Grid container direction="column" spacing={2}>
+      <Grid item>
+        <Grid container spacing={2} alignItems="center">
+          <Grid item>
+            <FlagIcon code={country.properties.iso2.toLowerCase()} size="2x" />
+          </Grid>
+          <Grid item>
+            <Typography variant="h5">{competition.properties.name}</Typography>
+          </Grid>
+        </Grid>
+        <Typography variant="subtitle1">
+          {country.properties.name}, {organizedIn.properties.city}
+        </Typography>
+      </Grid>
+      <Grid item>
+        {roundsWithEvent.map(({ round, event }) => (
+          <ExpansionPanel
+            key={round.identity}
+            TransitionProps={{ mountOnEnter: true }}
+          >
+            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography>
+                {event.properties.name}, Round{' '}
+                {round.properties.roundNumber.toInt()}
+              </Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <RoundResultsTable round={round} event={event} />
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+        ))}
+      </Grid>
+    </Grid>
+  );
 }
 
 export default Competition;
---
+++ client/src/components/RoundResultsTable.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import {
+  LinearProgress,
+  Table,
+  TableHead,
+  TableCell,
+  TableRow,
+  TableBody,
+} from '@material-ui/core';
+import { useQuery } from '../hooks/database';
+import { formatAttemptResult } from '../lib/formatters';
+
+const query = `
+  MATCH
+    (r:Round)<-[result:RESULT_IN]-(p:Person)
+  WHERE id(r) = $roundId
+  WITH result, p
+  ORDER BY result.rank
+  RETURN result, p AS person
+`;
+
+function RoundResultsTable({ round, event }) {
+  const { data, loading, error } = useQuery(query, { roundId: round.identity });
+
+  if (loading) return <LinearProgress />;
+  if (error) return error.message;
+
+  return (
+    <Table size="small">
+      <TableHead>
+        <TableRow>
+          <TableCell>#</TableCell>
+          <TableCell>Name</TableCell>
+          <TableCell colSpan={5}></TableCell>
+          <TableCell align="right">Average</TableCell>
+          <TableCell align="right">Best</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {data.map(({ result, person }) => (
+          <TableRow>
+            <TableCell>{result.properties.rank.toInt()}</TableCell>
+            <TableCell>{person.properties.name}</TableCell>
+            {result.properties.attempts.map((attempt) => (
+              <TableCell align="right">
+                {formatAttemptResult(attempt.toInt(), event.properties.wcaId)}
+              </TableCell>
+            ))}
+            <TableCell align="right" style={{ fontWeight: 600 }}>
+              {formatAttemptResult(
+                result.properties.average.toInt(),
+                event.properties.wcaId
+              )}
+            </TableCell>
+            <TableCell align="right">
+              {formatAttemptResult(
+                result.properties.best.toInt(),
+                event.properties.wcaId
+              )}
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
+export default RoundResultsTable;
